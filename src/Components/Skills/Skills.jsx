647cb1f8d07a04e3831fa86e36b3797ba8ae2de5
Nishu Priya <priyanishu0503@gmail.com>
2025-01-02 17:01:37 +0530

    changes

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -7,19 +7,27 @@ import mongodb from "../../assets/mongodb.png";
 import react from "../../assets/react.png";
 
 function Skills() {
-   console.log(html, css, javascript, mongodb, react);
+  console.log(html, css, javascript, mongodb, react);
   return (
     <div id="skills">
-      
-        <h1 className="skills">SKILLS</h1>
-        <div className="container">
-         <div className="item">
-        <Card title="HTML" image={html} />
-        <Card title="CSS" image={css} />
-        <Card title="JAVASCRIPT" image={javascript} />
-        <Card title="MONGODB" image={mongodb} />
-        <Card title="REACT" image={react} />
+      <div className="skiltxt">
+       <center> <h1 className="skills">SKILLS</h1></center>
+        {/* <hr className="hrr" /> */}
       </div>
+      <div className="container">
+        <div className="item">
+          <div className="one">
+            <Card title="HTML" image={html} />
+            <Card title="CSS" image={css} />
+          </div>
+          <div className="two">
+            <Card title="JAVASCRIPT" image={javascript} />
+            <Card title="MONGODB" image={mongodb} />
+          </div>
+          <div className="three">
+            <Card title="REACT" image={react} />
+          </div>
+        </div>
       </div>
     </div>
   );
