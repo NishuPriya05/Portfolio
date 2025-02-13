20bbd3d3f858837ac66f9ece94c3077a3cadc2d5
Nishu Priya <priyanishu0503@gmail.com>
2025-02-13 21:00:21 +0530

    portfolio

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import "./Projects.css";
 import ProjetsCard from "../ProjetsCard/ProjetsCard";
 import blog from "../../assets/blog.png";
+import styleme from "../../assets/styleme.png";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
@@ -48,6 +49,16 @@ const Projects = () => {
           main="Developed a dynamic blog application using React and Appwrite, featuring user authentication, real-time post creation with a rich text editor, and seamless functionality for adding and managing content. Integrated Appwrite's backend services for secure and efficient data handling."
           demoLink="https://techworld-blog.netlify.app/"
           sourceCode="https://github.com/NishuPriya05/Mega-Blog"
+        />
+        <ProjetsCard
+          title="Style Me"
+          image={styleme}
+          className="styleme"
+          main="StyleMe is a full-stack e-commerce web application designed to provide a seamless shopping experience. The platform features user authentication (login/signup), product filtering & sorting, an intuitive cart system, and a secure checkout process using Stripe. 
+
+The project includes a dedicated admin panel that allows administrators to add, update, and delete products, as well as manage user orders efficiently."
+          demoLink=" https://style-me-pi.vercel.app/"
+          sourceCode="https://github.com/NishuPriya05/Style-Me"
         />
       </div>
     </div>
